f8498507e7cbd1bc1cebd05b90ef25f851bf7c47
adisornliu003 <adisorn211liu@gmail.com>
2022-08-25 00:00:15 +0700

    Create app.js

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+let express = require('express')
+const app = express()
+
+app.get('status', function (req, res){
+    res.send('hello nodejs server')
+
+})
+
+let port = 8081
+
+app.listen(port, function () {
+    console.log('server running on port' + port)
+
+})
